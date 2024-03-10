c454782d925c415ea6bcb3cd8a57775f747ff80e
Doonn <Doonn3@gmail.com>
2024-03-10 14:16:01 +0300

    refactor: contrext store renamed

--- src/presentation/store/model/useStore.ts
+++ src/presentation/store/model/useStore.ts
@@ -1,45 +1,45 @@
-import { useFilter } from "../hooks/useFilter";
-import { usePageCount } from "../hooks/usePageCount";
+import { useSearchByFilter } from "../hooks/useSearchByFilter";
+import { usePages } from "../hooks/usePages";
 import { useProducts } from "../hooks/useProducts";
 
 export function useStore() {
   const products = useProducts();
-  const filter = useFilter();
-  const pageCount = usePageCount();
+  const searchByFilter = useSearchByFilter();
+  const pages = usePages();
 
   const init = () => {
-    filter.initFields();
+    searchByFilter.initFields();
     products.initProducts();
-    pageCount.initAmountPages();
+    pages.initAmountPages();
   };
 
   const getProducts = (pageNumber: number = 1) => {
-    if (filter.filterResult.length > 0) {
-      products.getProductsByIds(filter.filterResult[pageNumber - 1]);
+    if (searchByFilter.searchResult.length > 0) {
+      products.getProductsByIds(searchByFilter.searchResult[pageNumber - 1]);
     } else {
       products.initProducts(pageNumber);
     }
   };
 
   const searchProducts = async (type: string, value: string) => {
-    const resultSearch = await filter.search(type, value); // Возвращает массив айдишек в виду [][]
+    const resultSearch = await searchByFilter.search(type, value); // Возвращает массив айдишек в виду [][]
     if (!resultSearch) return;
 
-    pageCount.setPageCount(resultSearch.length);
+    pages.setTotalPageCount(resultSearch.length);
 
     products.getProductsByIds(resultSearch[0]);
   };
 
   const resetSearchResult = async () => {
-    filter.resetSearchResult();
-    pageCount.initAmountPages();
+    searchByFilter.resetSearchResult();
+    pages.initAmountPages();
     products.initProducts();
   };
 
   return {
     products,
-    filter,
-    pageCount,
+    searchByFilter,
+    pages,
     getProducts,
     searchProducts,
     resetSearchResult,
